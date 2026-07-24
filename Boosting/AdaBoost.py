3617c7618cc6e729d15b4dd447fa9707a787fc8b
venoprotective <venoprotective@gmail.com>
2026-07-24 23:45:45 +0300

    all adaboost class

--- Boosting/AdaBoost.py
+++ Boosting/AdaBoost.py
@@ -19,10 +19,17 @@ class AdaBoost:
         self.random_state = 0
         self.weights = []
         self.models = [] # stumps
+
+    def normalize(self, weights):
+        return weights / sum(weights)
+    
+    def update_weights(self, weights, y, tree_predictions, a_j_coeff):    
+         return weights[y != tree_predictions] * np.exp(a_j_coeff)
             
     def fit(self, X, y):
         n_samples = np.unique(y)
         weights = np.ones(n_samples) / n_samples
+        self.amount_classes = len(np.unique(y))
         for _ in range(self.n_estimators):
             tree = DecisionTreeClassifier(max_depth=3, random_state=self.random_state)
             tree = tree.fit(X, y, sample_weight=weights)
@@ -32,15 +39,21 @@ class AdaBoost:
             
             self.models.append(tree)
             self.weights.append(a_j_coeff)
-            weights = update_weights(weights, y, tree_predictions, a_j_coeff)
-            weights = normalize(weights)
-            
-    def normalize(self, weights):
-        return weights    
+            weights = self.update_weights(weights, y, tree_predictions, a_j_coeff)
+            weights = self.normalize(weights)
     
-    def update_weights(self, weights, y, tree_predictions, a_j_coeff):    
-         return weights[y != tree_predictions] * np.exp(a_j_coeff)
-         
+    def predict(self, X):
+        return self.majority_weighted_votes(X)
     
+    def majority_weighted_votes(self, X):
+        votes = np.zeros((len(X), self.amount_classes)) 
+        for j in range(self.n_estimators):
+            aj = self.weights[j]
+            Cj = self.models[j]
+            predictions = Cj.predict(X)
+            votes[np.array(range(len(X))),predictions] += aj
+    
+        return np.argmax(votes, axis=1)
+
           
         
