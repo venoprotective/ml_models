fce4411c573fa4523d781eea517f7b6ba5a4f633
venoprotective <venoprotective@gmail.com>
2026-07-25 00:09:47 +0300

    test withount cross validation

--- Boosting/AdaBoost.py
+++ Boosting/AdaBoost.py
@@ -16,26 +16,30 @@ class AdaBoost:
     def __init__(self, n_estimators=50, learning_rate=1.0, random_state=0):
         self.n_estimators = n_estimators
         self.learning_rate = learning_rate
-        self.random_state = 0
+        self.random_state = random_state
         self.weights = []
         self.models = [] # stumps
 
     def normalize(self, weights):
-        return weights / sum(weights)
+        return weights / np.sum(weights)
     
     def update_weights(self, weights, y, tree_predictions, a_j_coeff):    
-         return weights[y != tree_predictions] * np.exp(a_j_coeff)
-            
+        new_weights = weights.copy()
+        new_weights[y != tree_predictions] *= np.exp(a_j_coeff) # false -> maximize
+        new_weights[y == tree_predictions] *= np.exp(-a_j_coeff) # true -> minimize
+        
+        return new_weights
+                
     def fit(self, X, y):
         n_samples = len(y)
         weights = np.ones(n_samples) / n_samples
-        self.amount_classes = len(np.unique(y.values))
+        self.amount_classes = len(np.unique(y))
         for _ in range(self.n_estimators):
             tree = DecisionTreeClassifier(max_depth=3, random_state=self.random_state)
             tree = tree.fit(X, y, sample_weight=weights)
             tree_predictions = tree.predict(X)
-            epsilon = sum(self.weights[tree_predictions != y])
-            a_j_coeff = self.learning_rate * np.log((1 - epsilon) / epsilon)
+            epsilon = np.sum(weights[tree_predictions != y])
+            a_j_coeff = self.learning_rate * 0.5 *  np.log((1 - epsilon) / epsilon)
             
             self.models.append(tree)
             self.weights.append(a_j_coeff)
@@ -51,8 +55,9 @@ class AdaBoost:
             aj = self.weights[j]
             Cj = self.models[j]
             predictions = Cj.predict(X)
-            votes[np.array(range(len(X))),predictions] += aj
-    
+            for i in range(len(X)):
+                votes[i, predictions[i]] += aj
+
         return np.argmax(votes, axis=1)
 
           
